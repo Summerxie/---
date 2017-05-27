7d505b96a171c2c5277c12a252abe3252264a2ac
Summer <omychic@126.com>
2017-05-27 20:28:01 +0800

    Animation-cube
    可爱简单的转场动画

--- 02-核心动画-转场动画/AppDelegate.h
+++ 02-核心动画-转场动画/AppDelegate.h
@@ -3,7 +3,7 @@
 //  02-核心动画-转场动画
 //
 //  Created by apple on 17/2/17.
-//  Copyright © 2017年 itheima. All rights reserved.
+
 //
 
 #import <UIKit/UIKit.h>
--- 02-核心动画-转场动画/ViewController.m
+++ 02-核心动画-转场动画/ViewController.m
@@ -3,12 +3,18 @@
 //  02-核心动画-转场动画
 //
 //  Created by apple on 17/2/17.
-//  Copyright © 2017年 itheima. All rights reserved.
+
 //
 
 #import "ViewController.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) NSMutableArray *images;
+
+@property (nonatomic, assign) NSInteger index;
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
@@ -17,8 +23,48 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.index = 0;
+    
+    self.images = [NSMutableArray array];
+    
+    for (int i = 0; i < 6; i++) {
+        // 拼图片的名字
+        NSString *imageName = [NSString stringWithFormat:@"image%zd", i + 1];
+        UIImage *image = [UIImage imageNamed:imageName];
+        [self.images addObject:image];
+    }
 }
 
+- (IBAction)swipe:(UISwipeGestureRecognizer *)sender {
+    
+    CATransition *anim = [[CATransition alloc] init];
+    anim.type = @"cube";
+    
+    
+    
+    if (sender.direction == UISwipeGestureRecognizerDirectionLeft) { // 从右往左
+        self.index++;
+        
+        anim.subtype = kCATransitionFromRight;
+        
+        if(self.index == self.images.count) {
+            self.index = 0;
+        }
+    } else { // 从左往右
+        self.index--;
+        
+        anim.subtype = kCATransitionFromLeft;
+        
+        if(self.index == -1) {
+            self.index = self.images.count - 1;
+        }
+    }
+    
+    self.imageView.image = self.images[self.index];
+    
+    [self.imageView.layer addAnimation:anim forKey:nil];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
